7784d2b5a0a5b4b8cf1f02cbd053548642c130e2
Joseph Valle <josephrvalle@gmail.com>
2023-02-18 20:58:23 -0500

    switched the img links to event listeners, but there's an issue with window.open so let's re-visit this

--- script.js
+++ script.js
@@ -28,3 +28,92 @@ function checkTime(i) {
   if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
   return i;
 }
+
+// Links for all the albums.
+const lb = document.getElementById("lb-link");
+lb.addEventListener("click", () => {
+  window.open("https://wet.ffm.to/letterblue");
+})
+lb?.addEventListener("mouseover", () => {
+  lb.style.mixBlendMode = "difference";
+})
+lb?.addEventListener("mouseout", () => {
+  lb.style.mixBlendMode = "exclusion";
+})
+
+const tf = document.getElementById("tf-link");
+tf.addEventListener("click", () => {
+  window.open("http://li.sten.to/kUUK4b6");
+})
+tf?.addEventListener("mouseover", () => {
+  tf.style.mixBlendMode = "difference";
+})
+tf?.addEventListener("mouseout", () => {
+  tf.style.mixBlendMode = "exclusion";
+})
+
+const ob = document.getElementById("ob-link");
+ob.addEventListener("click", () => {
+  window.open("http://li.sten.to/USXnYiV");
+})
+ob?.addEventListener("mouseover", () => {
+  ob.style.mixBlendMode = "difference";
+})
+ob?.addEventListener("mouseout", () => {
+  ob.style.mixBlendMode = "exclusion";
+})
+
+const ss = document.getElementById("ss-link");
+ss.addEventListener("click", () => {
+  window.open("https://allwet.bandcamp.com/album/single-sound");
+})
+ss?.addEventListener("mouseover", () => {
+  ss.style.mixBlendMode = "difference";
+})
+ss?.addEventListener("mouseout", () => {
+  ss.style.mixBlendMode = "exclusion";
+})
+
+const sr = document.getElementById("sr-link");
+sr.addEventListener("click", () => {
+  window.open("http://li.sten.to/QjI5Zo8");
+})
+sr?.addEventListener("mouseover", () => {
+  sr.style.mixBlendMode = "difference";
+})
+sr?.addEventListener("mouseout", () => {
+  sr.style.mixBlendMode = "exclusion";
+})
+
+const tmta = document.getElementById("tmta-link");
+tmta.addEventListener("click", () => {
+  window.open("http://li.sten.to/S5ofp2p");
+})
+tmta?.addEventListener("mouseover", () => {
+  tmta.style.mixBlendMode = "difference";
+})
+tmta?.addEventListener("mouseout", () => {
+  tmta.style.mixBlendMode = "exclusion";
+})
+
+const dy = document.getElementById("dy-link");
+dy.addEventListener("click", () => {
+  window.open("http://li.sten.to/BJJlmaX", "_blank");
+})
+dy?.addEventListener("mouseover", () => {
+  dy.style.mixBlendMode = "difference";
+})
+dy?.addEventListener("mouseout", () => {
+  dy.style.mixBlendMode = "exclusion";
+})
+
+const ep = document.getElementById("ep-link");
+ep?.addEventListener("click", () => {
+  window.open("http://li.sten.to/uM72qSv");
+})
+ep?.addEventListener("mouseover", () => {
+  ep.style.mixBlendMode = "difference";
+})
+ep?.addEventListener("mouseout", () => {
+  ep.style.mixBlendMode = "exclusion";
+})
